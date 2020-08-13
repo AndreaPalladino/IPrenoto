7dbe6340dcc3c50a227741ef1adddbc38c3e4091
Andrea Palladino <andreagpalladino@gmail.com>
2020-08-13 19:31:42 +0200

    progetto impostato, scaffolding e bda fatti, creato database, creato controller pubblico e iniziata welcome

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,82 @@
+@extends('layouts.app')
+@section('style')
+    
+@endsection
+@section('content')
+    
+
+
+<header class="masthead">
+    <div class="container h-100">
+      <div class="row h-100 align-items-center">
+        <div class="col-12 text-left mt-5 pt-5">
+          <h1 class="font-weight-light text10">I<span class="h1 text-white">Prenoto</span></h1>
+          <p class="lead text-white">il portale per gesitre le <span class="p text10">tue prenotazioni</span></p>
+        </div>
+      </div>
+    </div>
+</header>
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-12 col-md-3 over  d-flex justify-content-center align-itens-center flex-column mx-4">
+            <h2 class="text-center text-white">2Mila</h2>
+            <hr>
+            <h2 class="text-center text-white">Utenti</h2>
+        </div>
+        <div class="col-12 col-md-3 over  d-flex justify-content-center align-itens-center flex-column mx-4">
+            <h2 class="text-center text-white">3Cento</h2>
+            <hr>
+            <h2 class="text-center text-white">Attività</h2>
+        </div>
+        <div class="col-12 col-md-3 over  d-flex justify-content-center align-itens-center flex-column mx-4">
+            <h2 class="text-center text-white">+5Mila</h2>
+            <hr>
+            <h2 class="text-center text-white">Prenotazioni</h2>
+        </div>
+    </div>
+</div>
+
+<div class="container my-3 py-3">
+    <div class="row justify-content-center">
+        <div class="col-12">
+            <h3 class="text-center">Chi Siamo</h3>
+            <hr>
+            <p class="text-justify">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam vitae dignissimos non beatae nemo illum, aliquid reiciendis. Facere ipsam sequi tenetur atque in, totam corrupti necessitatibus molestiae minus eaque voluptas.</p>
+            <p class="text-justify">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam vitae dignissimos non beatae nemo illum, aliquid reiciendis. Facere ipsam sequi tenetur atque in, totam corrupti necessitatibus molestiae minus eaque voluptas.</p>
+            <p class="text-justify">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam vitae dignissimos non beatae nemo illum, aliquid reiciendis. Facere ipsam sequi tenetur atque in, totam corrupti necessitatibus molestiae minus eaque voluptas.</p>
+        </div>
+    </div>
+</div>
+
+<div class="container-fluid my-5 py-3">
+    <div class="row">
+        <div class="col-12 col-md-6 arrow my-5 d-flex justify-content-center align-itens-center flex-column">
+            <h3 class="text-right text-white mr-5 pr-5">Hotel</h3>
+        </div>
+        <div class="col-12 col-md-6 mt-5 pt-4">
+            <a href="" class="mt-5 pt-5 ml-5 pl-3 h3 textHover">Vai all'elenco</a>
+        </div>
+        <div class="col-12 col-md-6 arrow mb-5 d-flex justify-content-center align-itens-center flex-column">
+            <h3 class="text-right text-white mr-5 pr-5">Ristoranti</h3>
+        </div>
+        <div class="col-12 col-md-6">
+            <div class="col-12 col-md-6 mt-4">
+                <a href="" class=" h3 mt-5 ml-5 textHover">Vai all'elenco</a>
+            </div>
+        </div>
+        <div class="col-12 col-md-6 arrow mb-5 d-flex justify-content-center align-itens-center flex-column">
+            <h3 class="text-right text-white mr-5 pr-5">Palestre</h3>
+        </div>
+        <div class="col-12 col-md-6">
+            <div class="col-12 col-md-6 mt-4">
+                <a href="" class=" h3 mt-5 ml-5 textHover">Vai all'elenco</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+
+Auth::routes();
+
+/* TUTTI */
+Route::get('/', 'PublicController@index')->name('homepage');
+
+
+/* UTENTI LOGGATI */
+Route::get('/home', 'HomeController@index')->name('home');
